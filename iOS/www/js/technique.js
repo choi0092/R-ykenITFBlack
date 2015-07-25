f40451f4c0d24c3829b79e619d123d03252a309a
Kyung Un Choi <iphoneappsbychoi@gmail.com>
2015-07-25 23:55:35 +0200

    Add more scripts and json configuration.

--- iOS/www/js/technique.js
+++ iOS/www/js/technique.js
@@ -2,23 +2,38 @@ $( ".technique" ).bind( "pageinit", function() {
 	   var id = $( this ).attr( "id" );
        var page = "#" + id; 
        var dataString = JSON.stringify(patternData);
-	   var jsonData = JSON.parse(dataString);
-	   //$(page + " #techInfo span.ui-btn-inner").text(jsonData.ChonJi.techniques[page]);
+	   
+       var jsonData = JSON.parse(dataString);
+       var lastPage = "#T" + jsonData.lastTechniqueIndex;
+ 
+       if(page == lastPage)
+                       {
+                        $(page + " .next").css("display", "none");
+                        $(page + " .next").attr('class', "");
+                       }
+       else if(page == "#T0")
+                       {
+                       $(page + " .prev").css("display", "none");
+                       $(page + " .prev").attr('class', "");
+                       }
+                      
                        
-        $( page + " #TechImg" ).attr('src', id + ".jpg");
+       $(page + " #techInfo span.ui-btn-inner").text(jsonData.techniques[page].name);
+                       
+       $( page + " #TechImg" ).attr('src', "./resources/" + jsonData.techniques[page].image);
                        
                        
-        $( page + " #TechImg" ).on("vclick", function() {
+       $( page + " #TechImg" ).on("vclick", function() {
                                  var video = document.getElementById('video');
                                  var source = document.createElement('source');
                                  video.removeChild(video.childNodes[0]);
-                                 source.setAttribute('src', id + '.mp4');
+                                 source.setAttribute('src', "./resources/" + jsonData.techniques[page].video);
                                  video.appendChild(source);
                                  video.load();
                                  video.play();
                                  });
        
-        $( document ).on( "swipeleft", page, function() {
+       $( document ).on( "swipeleft", page, function() {
                         $(page + " .next").click();
                         
                         });
@@ -30,7 +45,7 @@ $( ".technique" ).bind( "pageinit", function() {
                        
         
        $( page + " #techInfo a" ).on("vclick", function() {
-                        playAudio(id + '.mp3');
+                        playAudio("./resources/" + jsonData.techniques[page].audio);
                         });
 
 });
