63b2837fd504a3eb25f1c19ff5bfc4375c7e0927
Kyung Un Choi <iphoneappsbychoi@gmail.com>
2015-09-12 22:49:34 +0200

    ignoring platfroms contents from git.

--- iOS/platforms/ios/www/js/initialize.js
+++
@@ -1,8 +0,0 @@
-$.mobile.defaultPageTransition   = 'none';
-$.mobile.defaultDialogTransition = 'none';
-$.mobile.buttonMarkup.hoverDelay = 1; 
-
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-        navigator.splashscreen.hide();
-    }
